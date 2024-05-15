8695421e75a832d8d107c8990e36001cfc43199b
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-15 04:57:20 +0300

    squash!

--- .changes/fix-rs-codeblock.md
+++
@@ -1,5 +0,0 @@
----
-"rustdocusaurus": patch
----
-
-Generated codeblocks are now valid MD codeblocks with "rs" flavor.
--- utils/string.js
+++
@@ -1,5 +0,0 @@
-const capitalize = (item) => item.charAt(0).toUpperCase() + item.slice(1);
-
-module.exports = {
-  capitalize,
-};
